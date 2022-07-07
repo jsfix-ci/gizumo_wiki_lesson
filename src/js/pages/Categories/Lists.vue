af45d281ff302b23bcf90d35f75bc9137fbe96e3
sGvQs <susum.earth@gmail.com>
2022-07-07 11:41:25 +0900

    カテゴリ一管理画面作成　初回コミット

---
+++ src/js/pages/Categories/Lists.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="categories">
+    <div class="categories-post">
+      <app-category-post
+        :access="access"
+      />
+    </div>
+    <div class="categories-border" />
+    <div class="categories-list">
+      <app-category-list
+        :categories="categoryList"
+        :access="access"
+        :theads="thead"
+      />
+      <!-- <div class="leftIsProp"></div>
+      <div class="rightIsMethod"></div>
+      :deletCateoryName="sample"
+      -->
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { CategoryPost, CategoryList } from '@Components/molecules';
+
+export default {
+  components: {
+    appCategoryList: CategoryList,
+    appCategoryPost: CategoryPost,
+  },
+  data() {
+    return {
+      categories: '',
+      thead: ['カテゴリー名'],
+    };
+  },
+  computed: {
+    categoryList() {
+      return this.$store.state.categories.categoryList;
+    },
+    access() {
+      return this.$store.getters['auth/access'];
+    },
+  },
+  created() {
+    this.$store.dispatch('categories/getAllCategories');
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+  .categories {
+    display: flex;
+    &-post {
+      width: 29%;
+      margin-right: 1%;
+    }
+    &-border {
+      width: 0%;
+      border-left: 1px solid #ccc;
+    }
+    &-list {
+      width: 69%;
+      margin-left: 1%;
+    }
+  }
+</style>
