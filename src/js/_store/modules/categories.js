a6c383d4fc8d849f90db96e2abb8c5f4cc5dae50
琴畑一馬 <kotohatakazuma@kotohatakazumanoMacBook-Air.local>
2022-07-15 15:43:44 +0900

    axiosを使った編集機能の実装

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -9,13 +9,13 @@ export default {
       id: null,
       name: '',
     },
+    updateCategory: {
+      id: null,
+      name: '',
+    },
     doneMessage: '',
     errorMessage: '',
     isLoading: false,
-  },
-  getters: {
-    targetCategory: state => state.targetCategory,
-    deleteCategory: state => state.deleteCategory.id,
   },
   mutations: {
     // 全カテゴリー名の反映
@@ -30,6 +30,18 @@ export default {
     // 作成するカテゴリーの反映
     addTargetCategory(state, name) {
       state.targetCategory = name;
+    },
+    targetCategoryReset(state) {
+      state.targetCategory = '';
+    },
+    // 編集後のカテゴリー名取得
+    updateCategoryValue(state, name) {
+      state.updateCategory.name = name;
+    },
+    // 更新するカテゴリーの反映
+    getCategoryName(state, { id, getName }) {
+      state.updateCategory.id = id;
+      state.updateCategory.name = getName;
     },
     // ローディング状態の反映
     toggleLoading(state) {
@@ -62,26 +74,23 @@ export default {
         commit('failRequest', errTxt);
       });
     },
-    // 削除する要素
-    addDeleteCategory({ commit }, { categoryId: id, categoryName: name }) {
-      commit('addDeleteCategory', { id, name });
-    },
     // 追加する要素
     addTargetCategory({ commit }, name) {
       commit('addTargetCategory', name);
     },
     // カテゴリー作成
-    createCategory({ commit, rootGetters }) {
+    createCategory({ commit, rootGetters, state }) {
       return new Promise((resolve) => {
         commit('clearMessage');
         commit('toggleLoading');
         const data = new URLSearchParams();
-        data.append('name', rootGetters['categories/targetCategory']);
+        data.append('name', state.targetCategory);
         axios(rootGetters['auth/token'])({
           method: 'POST',
           url: '/category',
           data,
         }).then(() => {
+          commit('targetCategoryReset');
           commit('toggleLoading');
           commit('categoryDoneMessage', { message: 'カテゴリーを作成しました' });
           resolve();
@@ -91,16 +100,58 @@ export default {
         });
       });
     },
-    deleteCategory({ commit, rootGetters }) {
+    // 削除する要素
+    addDeleteCategory({ commit }, { categoryId: id, categoryName: name }) {
+      commit('addDeleteCategory', { id, name });
+    },
+    // カテゴリーの削除
+    deleteCategory({ commit, rootGetters, state }) {
       return new Promise((resolve) => {
         commit('clearMessage');
         axios(rootGetters['auth/token'])({
           method: 'DELETE',
-          url: `/category/${rootGetters['categories/deleteCategory']}`,
+          url: `/category/${state.deleteCategory.id}`,
         }).then(() => {
           commit('categoryDoneMessage', { message: 'カテゴリーを削除しました' });
           resolve();
         }).catch((err) => {
+          commit('failRequest', { message: err.message });
+        });
+      });
+    },
+    // 変更するカテゴリー名の取得
+    getCategoryName({ commit, rootGetters }, id) {
+      axios(rootGetters['auth/token'])({
+        method: 'GET',
+        url: `/category/${id}`,
+      }).then(({ data }) => {
+        const getName = data.category.name;
+        commit('getCategoryName', { id, getName });
+      }).catch((err) => {
+        commit('failRequest', { message: err.message });
+      });
+    },
+    // 編集後のカテゴリー名取得
+    updateCategoryValue({ commit }, name) {
+      commit('updateCategoryValue', name);
+    },
+    // カテゴリーの編集
+    updateCategory({ commit, rootGetters, state }) {
+      return new Promise((resolve) => {
+        commit('clearMessage');
+        commit('toggleLoading');
+        const data = new URLSearchParams();
+        data.append('name', state.updateCategory.name);
+        axios(rootGetters['auth/token'])({
+          method: 'PUT',
+          url: `/category/${state.updateCategory.id}`,
+          data,
+        }).then(() => {
+          commit('toggleLoading');
+          commit('categoryDoneMessage', { message: 'カテゴリー名を更新しました' });
+          resolve();
+        }).catch((err) => {
+          commit('toggleLoading');
           commit('failRequest', { message: err.message });
         });
       });
