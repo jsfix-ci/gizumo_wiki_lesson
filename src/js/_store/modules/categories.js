2d59ec71f3966bc4f7368846455a0251c2de0498
sGvQs <susum.earth@gmail.com>
2022-07-19 10:09:48 +0900

    feat: カテゴリ一新規作成機能実装

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -8,13 +8,71 @@ export default {
       name: '',
     },
     categoryList: [],
+    disabled: false,
+    doneMessage: '',
+    errorMessage: '',
   },
   mutations: {
     doneGetAllCategories(state, payload) {
       state.categoryList = [...payload.categories];
     },
+    getInputCategories(state, event) {
+      state.category.name = event;
+    },
+    toggleDisabled(state) {
+      state.disabled = !state.disabled;
+    },
+    deleteCategoryName(state) {
+      state.category.name = null;
+    },
+    displayDoneMessage(state) {
+      state.doneMessage = 'カテゴリー作成しました！';
+    },
+    deleteDisplayedMessage(state) {
+      state.doneMessage = null;
+      state.errorMessage = null;
+    },
+    displayErrorMessage(state) {
+      state.errorMessage = 'すでに存在してるカテゴリーです！';
+    },
   },
   actions: {
+    postCategory({ state, commit, rootGetters }) {
+      return new Promise((resolve, reject) => {
+        commit('toggleDisabled');
+        commit('deleteDisplayedMessage');
+        axios(rootGetters['auth/token'])({
+          method: 'POST',
+          url: '/category',
+          data: {
+            name: state.category.name,
+          },
+        }).then(() => {
+          resolve();
+          commit('toggleDisabled');
+          commit('deleteCategoryName');
+          commit('displayDoneMessage');
+          axios(rootGetters['auth/token'])({
+            method: 'GET',
+            url: '/category',
+          })
+            .then((res) => {
+              const payload = {
+                categories: res.data.categories.reverse(),
+              };
+              commit('doneGetAllCategories', payload);
+            })
+            .catch((err) => {
+              commit('failRequest', { message: err.message });
+            });
+        }).catch((err) => {
+          console.log({ message: err.message });
+          commit('toggleDisabled');
+          commit('displayErrorMessage');
+          reject();
+        });
+      });
+    },
     getAllCategories({ commit, rootGetters }) {
       axios(rootGetters['auth/token'])({
         method: 'GET',
@@ -29,6 +87,12 @@ export default {
         .catch((err) => {
           commit('failRequest', { message: err.message });
         });
+    },
+    getInputCategories({ commit }, event) {
+      commit('getInputCategories', event);
+    },
+    deleteDisplayedMessage({ commit }) {
+      commit('deleteDisplayedMessage');
     },
   },
 };
