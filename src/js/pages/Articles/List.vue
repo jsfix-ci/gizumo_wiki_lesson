c25994a5d7d8aecd5c0984096fd6189a1cb30e94
山本建治 <kenjibasketball0520@gmail.com>
2022-08-18 15:55:53 +0900

    add:methods,computedの追加

--- src/js/pages/Articles/List.vue
+++ src/js/pages/Articles/List.vue
@@ -9,28 +9,72 @@
       @openModal="openModal"
       @handleClick="handleClick"
     />
+    <app-page-nation
+      :show-pages="showPages"
+      :current-pages="currentPages"
+      :last-page="lastPage"
+      :current-page="currentPage"
+      @nextPage="fetchArticles"
+    />
   </div>
 </template>
 
 <script>
-import { ArticleList } from '@Components/molecules';
+import { ArticleList, PageNation } from '@Components/molecules';
 import Mixins from '@Helpers/mixins';
 
 export default {
   components: {
     appArticleList: ArticleList,
+    appPageNation: PageNation,
   },
   mixins: [Mixins],
   beforeRouteUpdate(to, from, next) {
-    this.fetchArticles();
     next();
   },
   data() {
     return {
       title: 'すべて',
+      showPages: 2,
     };
   },
   computed: {
+    currentPage() {
+      return this.$store.state.articles.pages.currentPage;
+    },
+    currentPages() {
+      const targetPage = this.$store.state.articles.pages.currentPage;
+      if (targetPage === 1 || targetPage === 2 || targetPage === 3) {
+        const targetPages = [2, 3, 4, 5, 6];
+        return targetPages;
+      }
+      const targetLastPage = this.$store.state.articles.pages.lastPage;
+      if (
+        targetLastPage === targetPage
+        || targetLastPage - 1 === targetPage
+        || targetLastPage - 2 === targetPage
+      ) {
+        const targetPages = [
+          targetLastPage - 5,
+          targetLastPage - 4,
+          targetLastPage - 3,
+          targetLastPage - 2,
+          targetLastPage - 1,
+        ];
+        return targetPages;
+      }
+      const targetPages = [
+        targetPage - 2,
+        targetPage - 1,
+        targetPage,
+        targetPage + 1,
+        targetPage + 2,
+      ];
+      return targetPages;
+    },
+    lastPage() {
+      return this.$store.state.articles.pages.lastPage;
+    },
     articlesList() {
       return this.$store.state.articles.articleList;
     },
@@ -42,7 +86,8 @@ export default {
     },
   },
   created() {
-    this.fetchArticles();
+    const pageQuery = this.$route.query.page;
+    this.fetchArticles(pageQuery);
   },
   methods: {
     openModal(articleId) {
@@ -67,7 +112,7 @@ export default {
         this.$store.dispatch('articles/getAllArticles');
       }
     },
-    fetchArticles() {
+    fetchArticles(page) {
       if (this.$route.query.category) {
         const { category } = this.$route.query;
         this.title = category;
@@ -80,7 +125,9 @@ export default {
             // console.log(err);
           });
       } else {
-        this.$store.dispatch('articles/getAllArticles');
+        this.$router.push({ query: { page } });
+        const pageQuery = this.$route.query.page;
+        this.$store.dispatch('articles/getAllArticles', pageQuery);
       }
     },
   },
