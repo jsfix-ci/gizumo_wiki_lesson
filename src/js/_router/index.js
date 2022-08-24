ea3215c0d62d1778598ff26f650e0b73495ecd2b
山本建治 <kenjibasketball0520@gmail.com>
2022-08-24 14:27:07 +0900

    add:router追加

--- src/js/_router/index.js
+++ src/js/_router/index.js
@@ -18,6 +18,7 @@ import ArticleList from '@Pages/Articles/List';
 import ArticleDetail from '@Pages/Articles/Detail';
 import ArticleEdit from '@Pages/Articles/Edit';
 import ArticlePost from '@Pages/Articles/Post';
+import ArticleTrashed from '@Pages/Articles/Trashed';
 
 // 自分のアカウントページ
 import Profile from '@Pages/Profile';
@@ -115,6 +116,11 @@ const router = new VueRouter({
           name: 'articlePost',
           path: 'post',
           component: ArticlePost,
+        },
+        {
+          name: 'articleTrashed',
+          path: 'trashed',
+          component: ArticleTrashed,
         },
         {
           name: 'articleDetail',
