bf48ec16fe55b8232f3694bf9d78ffc476a7aa06
山本建治 <kenjibasketball0520@gmail.com>
2022-08-24 15:32:22 +0900

    add:削除記事一覧ボタンの追加

--- src/js/components/molecules/ArticleList/index.vue
+++ src/js/components/molecules/ArticleList/index.vue
@@ -16,6 +16,18 @@
     >
       新しいドキュメントを作る
     </app-router-link>
+    <app-router-link
+      to="articles/trashed"
+      key-color
+      white
+      bg-lightgreen
+      small
+      round
+      hover-opacity
+      class="article-list__create-link article-list__trashed"
+    >
+      削除記事一覧
+    </app-router-link>
     <transition-group
       class="article-list__articles"
       name="fade"
@@ -160,6 +172,9 @@ export default {
     }
     &__create-link {
       margin-top: 16px;
+    }
+    &__trashed {
+      margin-left: 10px;
     }
     &__links {
       *:not(first-child) {
