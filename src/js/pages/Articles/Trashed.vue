18354409ec376798729225e32493784d6f5ba5c3
山本建治 <kenjibasketball0520@gmail.com>
2022-08-24 14:28:07 +0900

    add:trashedcomponent追加

---
+++ src/js/pages/Articles/Trashed.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <div>
+    <app-article-trashed
+      :theads="theads"
+      :title="title"
+      :target-array="targetArray"
+    />
+  </div>
+</template>
+<script>
+import {
+  ArticleTrashed,
+} from '@Components/molecules';
+
+export default {
+  components: {
+    appArticleTrashed: ArticleTrashed,
+  },
+  data() {
+    return {
+      title: '削除済み記事一覧',
+      theads: [
+        'タイトル',
+        '本文',
+        '作成日',
+      ],
+    };
+  },
+  computed: {
+    targetArray() {
+      return this.$store.state.articles.trashedArray;
+    },
+  },
+  created() {
+    this.setTrashedArticles();
+  },
+  methods: {
+    setTrashedArticles() {
+      this.$store.dispatch('articles/getTrashedArticles');
+    },
+  },
+};
+</script>
+<style lang="postcss" scoped>
+
+</style>
