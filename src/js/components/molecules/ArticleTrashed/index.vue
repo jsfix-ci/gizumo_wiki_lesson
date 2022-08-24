3859674b0e1a3e7ef8a79dc899cf3d9fa14a9134
山本建治 <kenjibasketball0520@gmail.com>
2022-08-24 14:27:54 +0900

    add:pagesにtrashed追加

---
+++ src/js/components/molecules/ArticleTrashed/index.vue
@@ -0,0 +1,158 @@
+<template lang="html">
+  <div class="trashed-list">
+    <app-heading :level="1">{{ title }}</app-heading>
+    <app-router-link
+      to="/articles"
+      key-color
+      white
+      bg-lightgreen
+      small
+      round
+      hover-opacity
+      class="trashed-list__create-link"
+    >
+      すべての記事一覧へ戻る
+    </app-router-link>
+    <table class="trashed-list__table">
+      <thead class="trashed-list__thead">
+        <tr>
+          <th
+            v-for="(thead, index) in theads"
+            :key="index"
+          >
+            <app-text
+              tag="span"
+              theme-color
+              bold
+            >
+              {{ thead }}
+            </app-text>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="trashed-list__tbody">
+        <tr v-for="article in filteredArticles" :key="article.id">
+          <td class="trashed-list__title">
+            <app-text
+              tag="span"
+              small
+            >
+              {{ article.title }}
+            </app-text>
+          </td>
+          <td class="trashed-list__content">
+            <app-text tag="span" small>{{ article.content }}</app-text>
+          </td>
+          <td class="trashed-list__time">
+            <app-text tag="span" small>{{ article.created_at }}</app-text>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import {
+  Heading,
+  Text,
+  RouterLink,
+} from '@Components/atoms';
+
+export default {
+  components: {
+    appHeading: Heading,
+    appText: Text,
+    appRouterLink: RouterLink,
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'すべて',
+    },
+    targetArray: {
+      type: Array,
+      default: () => [],
+    },
+    theads: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    filteredArticles() {
+      const filteredTitle = (this.targetArray).map(
+        (article) => {
+          const titleLength = article.title.length;
+          const thisText = article.title;
+          if (titleLength >= 30) {
+            const showText = thisText.slice(0, 30);
+            const showTitle = `${showText}…`;
+            return { ...article, title: showTitle };
+          }
+          return article;
+        },
+      );
+      const filteredContent = (filteredTitle).map(
+        (article) => {
+          const contentLength = article.content.length;
+          const thisText = article.content;
+          if (contentLength >= 30) {
+            const showText = thisText.slice(0, 30);
+            const showContent = `${showText}…`;
+            return { ...article, content: showContent };
+          }
+          return article;
+        },
+      );
+      const filteredTime = (filteredContent).map(
+        (article) => {
+          const thisText = article.created_at;
+          const showTime = thisText.slice(0, 10);
+          return { ...article, created_at: showTime };
+        },
+      );
+      return filteredTime;
+    },
+  },
+};
+</script>
+<style lang="postcss" scoped>
+
+  .trashed-list {
+    &__create-link {
+      margin-top: 10px;
+    }
+    &__table {
+      margin-top: 10px;
+      width: 100%;
+    }
+    tr {
+    border-bottom: 1px solid var(--separatorColor);
+    }
+    &__articles {
+      margin-top: 16px;
+    }
+    &__thead {
+      text-align: left;
+      th {
+        padding: 5px 10px;
+        vertical-align: middle;
+      }
+    }
+    &__tbody {
+      td {
+        padding: 10px;
+        vertical-align: middle;
+      }
+    }
+    &__title {
+      width: 35%;
+    }
+    &__content {
+      width: 55%;
+    }
+    &__time {
+      width: 10%;
+    }
+  }
+</style>
