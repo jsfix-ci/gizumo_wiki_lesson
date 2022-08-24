12120557579c827d34ac8f548724bd3de8a37eb4
山本建治 <kenjibasketball0520@gmail.com>
2022-08-24 16:32:48 +0900

    add:削除記事がないときのエラー文言追加

--- src/js/pages/Articles/Trashed.vue
+++ src/js/pages/Articles/Trashed.vue
@@ -1,20 +1,29 @@
 <template lang="html">
   <div>
     <app-article-trashed
+      v-if="targetArray"
       :theads="this.$options.constants.theads"
       :title="this.$options.constants.title"
       :target-array="targetArray"
+    />
+    <app-notice
+      v-if="!targetArray"
+      notice-text="削除された記事はありません。"
+      link-path="/articles"
+      link-text="記事一覧に戻る"
     />
   </div>
 </template>
 <script>
 import {
   ArticleTrashed,
+  Notice,
 } from '@Components/molecules';
 
 export default {
   components: {
     appArticleTrashed: ArticleTrashed,
+    appNotice: Notice,
   },
   constants: {
     title: '削除済み記事一覧',
