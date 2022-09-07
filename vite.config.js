c98550eaf53b6389afdeeceffc0fb70d6ea58947
keisuke3 <ishii@gizumo-inc.jp>
2022-09-08 00:22:37 +0900

    chore: 環境変数の設定とワークスペースの設定追加

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,57 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import legacy from '@vitejs/plugin-legacy';
 import checker from 'vite-plugin-checker';
 
-export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  plugins: [
-    createVuePlugin(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    }),
-    // checker({
-    //   eslint: {
-    //     lintCommand: 'eslint "js/**/*.{js,vue}"'
-    //   },
-    // }),
-  ],
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@Components': `${__dirname}/src/components`,
-      '@Helpers': `${__dirname}/src/js/_helpers`,
-      '@Pages': `${__dirname}/src/components/pages`,
-      '@Styles': `${__dirname}/src/styles`,
+export default (({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    root: 'src',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
     },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        require('autoprefixer'),
-      ],
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use '@Styles/scss/_utils/_utils.scss' as *;`,
+    plugins: [
+      createVuePlugin(),
+      legacy({
+        targets: ['ie >= 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      }),
+      // checker({
+      //   eslint: {
+      //     lintCommand: 'eslint "js/**/*.{js,vue}"'
+      //   },
+      // }),
+    ],
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        '@Components': `${__dirname}/src/components`,
+        '@Helpers': `${__dirname}/src/js/_helpers`,
+        '@Pages': `${__dirname}/src/components/pages`,
+        '@Styles': `${__dirname}/src/styles`,
       },
     },
-  },
-  server: {
-    open: true,
-    port: 8000,
-  },
+    css: {
+      postcss: {
+        plugins: [
+          require('autoprefixer'),
+        ],
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use '@Styles/scss/_utils/_utils.scss' as *;`,
+        },
+      },
+    },
+    server: {
+      open: true,
+      port: 8000,
+    },
+    define: {
+      API_BASE_URL: JSON.stringify(env.API_BASE_URL),
+      MY_SESSION_STORAGE_KEY: JSON.stringify(env.MY_SESSION_STORAGE_KEY),
+    },
+  });
 });
