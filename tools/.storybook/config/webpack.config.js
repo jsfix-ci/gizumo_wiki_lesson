8e7bc04ef3edd81117c9e4374e4d244b26cd2477
sanpicule <sannsi4444@gmail.com>
2022-09-27 12:02:51 +0900

    first commit

--- tools/.storybook/config/webpack.config.js
+++ tools/.storybook/config/webpack.config.js
@@ -1,61 +1,57 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = async({config, mode}) => {
+module.exports = async ({ config, mode }) => {
   // const vueLoader = config.module.rules.find(({ test }) => {
   //   return test.toString() === '/\\.vue$/'
   // });
 
   config.module.rules.push({
     test: /\.stories\.jsx?$/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre',
+    loaders: [require.resolve("@storybook/addon-storysource/loader")],
+    enforce: "pre",
   });
-
 
   config.module.rules.push({
     test: /\.(css|sass|scss)$/,
     use: [
       {
-        loader: 'vue-style-loader',
+        loader: "vue-style-loader",
       },
       {
-        loader: 'css-loader',
+        loader: "css-loader",
       },
       {
-        loader: 'postcss-loader',
+        loader: "postcss-loader",
         options: {
           plugins: [
-            require('autoprefixer')({
+            require("autoprefixer")({
               grid: true,
-              browsers: [
-                'IE >= 9',
-                'last 2 versions'
-              ]
-            })
-          ]
-        }
+              browsers: ["IE >= 9", "last 2 versions"],
+            }),
+          ],
+        },
       },
       {
-        loader: 'sass-loader',
+        loader: "sass-loader",
         options: {
           sourceMap: true,
           //変数やmixin定義の読み込み
           data: `@import '_helpers/index.scss';`,
-          includePaths: [path.resolve(__dirname, '../../../src/scss/')],
+          includePaths: [path.resolve(__dirname, "../../../src/scss/")],
         },
       },
-    ]
+    ],
   });
 
   const alias = {
-    '@Components': path.resolve(__dirname, '../../../src/js/components'),
-    '@Helpers': path.resolve(__dirname, '../../../src/js/_helpers'),
-    '@Pages': path.resolve(__dirname, '../../../../src/js/pages'),
-    '@Scss': path.resolve(__dirname, '../../../src/scss'),
-  }
+    "@Components": path.resolve(__dirname, "../../../src/js/components"),
+    "@Helpers": path.resolve(__dirname, "../../../src/js/_helpers"),
+    "@Pages": path.resolve(__dirname, "../../../../src/js/pages"),
+    "@Scss": path.resolve(__dirname, "../../../src/scss"),
+  };
 
-  config.resolve.alias = {...config.resolve.alias, ...alias};
-  config.resolve.extensions.push('.scss');
+  config.resolve.alias = { ...config.resolve.alias, ...alias };
+  config.resolve.extensions.push(".scss");
 
   return config;
-}
+};
