8164ada42789ad7a708e073d211837c53e78d64e
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-09-28 17:10:07 +0900

    カテゴリ一覧機能実装

--- src/js/_router/index.js
+++ src/js/_router/index.js
@@ -7,6 +7,10 @@ import Signout from '@Pages/Signout/index.vue';
 import NotFound from '@Pages/NotFound/index.vue';
 import Home from '@Pages/Home/index.vue';
 
+// カテゴリー
+import Categories from '@Pages/Categories/index.vue';
+import CategoryManagement from '@Pages/Categories/CategoryManagement.vue';
+
 // 記事
 import Articles from '@Pages/Articles/index.vue';
 import ArticleList from '@Pages/Articles/List.vue';
@@ -66,6 +70,17 @@ const router = new VueRouter({
       name: 'profile',
       path: '/profile',
       component: Profile,
+    },
+    {
+      path: '/categories',
+      component: Categories,
+      children: [
+        {
+          name: 'CategoryManagement',
+          path: '',
+          component: CategoryManagement,
+        },
+      ],
     },
     {
       path: '/articles',
---
+++ src/js/_store/modules/categories.js
@@ -0,0 +1,33 @@
+import axios from '@Helpers/axiosDefault';
+
+export default {
+  namespaced: true,
+  state: {
+    categoryList: [],
+    errorMessage: '',
+  },
+  getters: {
+  },
+
+  mutations: {
+    setCategoryList(state, payload) {
+      state.categoryList = payload;
+    },
+    failRequest(state, { message }) {
+      state.errorMessage = message;
+    },
+  },
+
+  actions: {
+    getAllCategories({ commit, rootGetters }) {
+      axios(rootGetters['auth/token'])({
+        method: 'GET',
+        url: '/category',
+      }).then(res => {
+        commit('setCategoryList', res.data.categories.reverse());
+      }).catch(err => {
+        commit('failRequest', { message: err.message });
+      });
+    },
+  },
+};
