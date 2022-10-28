170180157237315657b258fda6bdf5f9abc6c5a6
github　ar9611 <github>
2022-10-28 11:23:45 +0900

    feat: 追加したファイルをimport

--- src/js/_router/index.js
+++ src/js/_router/index.js
@@ -14,6 +14,10 @@ import ArticleDetail from '@Pages/Articles/Detail.vue';
 import ArticleEdit from '@Pages/Articles/Edit.vue';
 import ArticlePost from '@Pages/Articles/Post.vue';
 
+// カテゴリー
+import Categories from '@Pages/Categories/index.vue';
+import CategoriesList from '@Pages/Categories/List.vue';
+
 // 自分のアカウントページ
 import Profile from '@Pages/Profile/index.vue';
 
@@ -66,6 +70,17 @@ const router = new VueRouter({
       name: 'profile',
       path: '/profile',
       component: Profile,
+    },
+    {
+      path: '/categories',
+      component: Categories,
+      children: [
+        {
+          name: 'categoriesList',
+          path: '',
+          component: CategoriesList,
+        },
+      ],
     },
     {
       path: '/articles',
