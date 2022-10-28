b82ca42b3ea16b55382105b76e545ed6a14a301a
github　ar9611 <github>
2022-10-28 12:55:47 +0900

    add:カテゴリー一覧表示

---
+++ src/components/pages/Categories/List.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="category">
+    <app-category-post
+      class="category-post"
+      :access="access"
+    />
+    <app-category-list
+      class="category-list"
+      :categories="CategoriesList"
+      :access="access"
+    />
+  </div>
+</template>
+
+<script>
+import { CategoryList, CategoryPost } from '@Components/molecules';
+
+export default {
+  components: {
+    appCategoryList: CategoryList,
+    appCategoryPost: CategoryPost,
+  },
+  computed: {
+    CategoriesList() {
+      return this.$store.state.categories.categoryList;
+    },
+    access() {
+      return this.$store.getters['auth/access'];
+    },
+  },
+  created() {
+    this.$store.dispatch('categories/getAllCategories');
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.category {
+  display: flex;
+  height: 100%;
+  &-post {
+    padding-right: 2%;
+    width: 40%;
+    border-right: 1px solid #ccc;
+  }
+  &-list {
+    margin-left: 2%;
+    width: 60%;
+    overflow-y: scroll;
+    background-color: #fff;
+  }
+}
+</style>
