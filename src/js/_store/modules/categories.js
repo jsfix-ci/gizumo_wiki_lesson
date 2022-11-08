0365fc17e898885ff72da38bf491fe7d8742db18
kosuke-codeman3 <siratamatabetai@gmail.com>
2022-11-08 16:55:13 +0900

    feat:categoryのstore追加

---
+++ src/js/_store/modules/categories.js
@@ -0,0 +1,20 @@
+import axios from '@Helpers/axiosDefault';
+
+export default {
+  actions: {
+    // カテゴリー一覧取得
+    getAllCategories({ commit, rootGetters }) {
+      axios(rootGetters['auth/token'])({
+        method: 'GET',
+        url: '/category',
+      }).then(res => {
+        const payload = {
+          categories: res.data,
+        };
+        commit('doneGetAllCategories', payload);
+      }).catch(err => {
+        commit('failRequest', { message: err.message });
+      });
+    },
+  },
+};
