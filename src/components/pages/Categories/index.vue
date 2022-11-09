7cfd7182f55d9b508814010b33a49d30ae644514
kosuke-codeman3 <siratamatabetai@gmail.com>
2022-11-09 14:36:54 +0900

    feat:Categpry一覧取得APIの呼び出し処理作成

--- src/components/pages/Categories/index.vue
+++ src/components/pages/Categories/index.vue
@@ -1,15 +1,50 @@
 <template>
-  <app-category-post />
+  <div class="wrapper">
+    <app-category-post
+      :access="access"
+      :error-message="errorMessage"
+    />
+    <app-category-list
+      :categories="categories"
+      :theads="theads"
+      :access="access"
+    />
+  </div>
 </template>
 
 <script>
-import { CategoryPost } from '@Components/molecules';
+import { CategoryPost, CategoryList } from '@Components/molecules';
 
 export default {
   components: {
     appCategoryPost: CategoryPost,
+    appCategoryList: CategoryList,
+  },
+  data() {
+    return {
+      theads: ['カテゴリー名'],
+    };
   },
   computed: {
+    category() {
+      return this.$store.state.categories.categories;
+    },
+    access() {
+      return this.$store.getters['auth/access'];
+    },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+  },
+  created() {
+    this.$store.dispatch('categories/getAllCategories');
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display:flex;
+  justify-content: space-between;
+}
+</style>
--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -1,6 +1,37 @@
 import axios from '@Helpers/axiosDefault';
 
 export default {
+  namespaced: true,
+  state: {
+    category: {
+      id: null,
+      name: null,
+    },
+    links: {
+      first: null,
+      last: null,
+      prev: null,
+      next: null,
+    },
+    meta: {
+      current_page: null,
+      from: null,
+      last_page: null,
+      path: null,
+      per_page: null,
+      to: null,
+      total: null,
+    },
+    errorMessage: '',
+  },
+  mutations: {
+    failRequest(state, { message }) {
+      state.errorMessage = message;
+    },
+    doneGetAllCategories(state, payload) {
+      state.categories = { ...state.categories, ...payload.categories };
+    },
+  },
   actions: {
     // カテゴリー一覧取得
     getAllCategories({ commit, rootGetters }) {
