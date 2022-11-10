dd502d416f501d42941ac8559d8d821a39bdb6a8
kosuke-codeman3 <siratamatabetai@gmail.com>
2022-11-10 11:16:10 +0900

    fix:不必要なstateの削除

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -4,21 +4,6 @@ export default {
   namespaced: true,
   state: {
     category: {},
-    links: {
-      first: null,
-      last: null,
-      prev: null,
-      next: null,
-    },
-    meta: {
-      current_page: null,
-      from: null,
-      last_page: null,
-      path: null,
-      per_page: null,
-      to: null,
-      total: null,
-    },
     errorMessage: '',
   },
   mutations: {
